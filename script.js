9ff14b6e7da8e82f037156d119ab1f030151c6f3
Raaavula <88527139+raaavula222@users.noreply.github.com>
2025-05-08 21:50:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    const heartContainer = document.querySelector('.heart-container');
+
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (Math.random() * 3 + 3) + 's';
+    heartContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+}
+
+setInterval(createHeart, 300);
+
     window.addEventListener("scroll", onScroll);
     onScroll();
 });
